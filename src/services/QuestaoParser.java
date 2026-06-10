1c9c2b6e98feccfab8670ecf009260eee7c32102
Henri Salarolli <henribrazuca@gmail.com>
2026-06-10 16:29:18 -0300

    feat: add metodo de mapear Dificuldade na classe QuestaoParser.java

--- src/services/QuestaoParser.java
+++ src/services/QuestaoParser.java
@@ -1,4 +1,21 @@
 package services;
 
+import com.google.gson.*;
+import entities.*;
+import enums.Dificuldade;
+
+import java.io.*;
+import java.util.*;
+
 public class QuestaoParser {
+
+    private Dificuldade mapearDificuldade(String difficulty) {
+        return switch (difficulty) {
+            case "easy"   -> Dificuldade.FACIL;
+            case "medium" -> Dificuldade.MEDIO;
+            case "hard"   -> Dificuldade.DIFICIL;
+            default       -> Dificuldade.FACIL;
+        };
+    }
+
 }
